e9c708ef1a82313b2ea1d8498309033498954382
briciotimot <132694769+briciotimot@users.noreply.github.com>
2024-05-24 18:31:55 -0300

    Desafio de código

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-let nome = "Fabrício"
+let nome = "Éric"
 
-console.log(nome)
+let xp = 100
+
+
+if (xp <= 1000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Ferro!`)
+} else if (xp =>1001 && xp <= 2000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Bronze!`)
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Prata!`)
+} else if (xp >= 5001 && xp <= 7000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Ouro!`)
+} else if (xp >= 7001 && xp <= 8000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Platina!`)
+} else if (xp >= 8001 && xp <= 9000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Ascendente!`)
+} else if (xp >= 9001 && xp <= 10000) {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Imortal!`)
+} else {
+    console.log(`O Herói de nome: ${nome} está no Nível de: Radiante!`)
+}
